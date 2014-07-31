69e5d072ade95c9ffd693a409bfd98c08ebf844b
Kai Sun <tonysun329@gmail.com>
2014-07-31 21:47:15 +0800

    use before_action on find_group

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,16 @@
 class PostsController < ApplicationController
 
+	before_action :find_group
+
 	def new
-		@group = Group.find(params[:group_id])
 		@post = @group.posts.new
 	end
 
 	def edit
-		@group = Group.find(params[:group_id])
 		@post = @group.posts.find(params[:id])
-		
 	end
 
 	def create
-		@group = Group.find(params[:group_id])
 		@post = @group.posts.new(post_params)
 
 		if @post.save
@@ -23,7 +21,6 @@ class PostsController < ApplicationController
 	end
 
 	def update
-		@group = Group.find(params[:group_id])
 		@post = @group.posts.find(params[:id])
 
 		if @post.update(post_params)
@@ -34,7 +31,6 @@ class PostsController < ApplicationController
 	end
 
 	def destroy
-		@group = Group.find(params[:group_id])
 		@post = @group.posts.find(params[:id])
 
 		@post.destroy
@@ -45,6 +41,10 @@ class PostsController < ApplicationController
 
 	def post_params
 		params.require(:post).permit(:context)
+	end
+
+	def find_group
+		@group = Group.find(params[:group_id])
 	end
 
 end
