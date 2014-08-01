325791cb5f9dbe8a267d2d7d984f755c7a0dcef5
Kai Sun <tonysun329@gmail.com>
2014-08-01 18:22:37 +0800

    complete simple backstage management system

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ActiveRecord::Base
 
 	validates :title, :presence => true
 
-	has_many :posts
+	has_many :posts, :dependent => :destroy
 	has_many :group_users
 	has_many :member, :through => :group_users, :source => :user
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
 
+  namespace :account do
+    resources :groups
+    resources :posts
+  end
+
   devise_for :users
   resources :groups do
     member do
       post :join
       post :quit
     end
-    
+
     resources :posts
   end
 
